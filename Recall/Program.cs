08e35599f9b67ed71d2a6a1abb8bce3763be5835
wmadwand <wmadwand@gmail.com>
2018-11-27 10:23:25 +0300

    Recall

---
+++ Recall/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace Recall
+{
+    public class Receiver
+    {
+        public bool bankPayment;
+        public bool paypalPayment;
+
+        public Receiver(bool bankPayment, bool paypalPayment)
+        {
+            this.bankPayment = bankPayment;
+            this.paypalPayment = paypalPayment;
+        }
+    }
+
+    public abstract class PaymentHandler
+    {
+        protected PaymentHandler successor;
+
+        public void SetSuccessor(PaymentHandler successor)
+        {
+            this.successor = successor;
+        }
+
+        public virtual void Handle(Receiver receiver)
+        {
+            successor?.Handle(receiver);
+        }
+    }
+
+    public class BankPaymentHandler : PaymentHandler
+    {
+        public override void Handle(Receiver receiver)
+        {
+            if (receiver.bankPayment)
+            {
+                Console.WriteLine("BankPayment ok");
+            }
+            else
+            {
+                base.Handle(receiver); // successor?.Handle(receiver);
+            }
+        }
+    }
+
+    public class PayPalPaymentHandler : PaymentHandler
+    {
+        public override void Handle(Receiver receiver)
+        {
+            if (receiver.paypalPayment)
+            {
+                Console.WriteLine("PayPalPayment ok");
+            }
+            else
+            {
+                successor?.Handle(receiver);
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Receiver receiver = new Receiver(true, false);
+
+            PaymentHandler bankP = new BankPaymentHandler();
+            PaymentHandler paypP = new PayPalPaymentHandler();
+
+            bankP.SetSuccessor(paypP);
+            bankP.Handle(receiver);
+        }
+    }
+}
